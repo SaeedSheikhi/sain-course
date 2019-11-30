8b68ef5350e86c01ba0ae9709e06f9e05604f074
SaeedSheikhi <SaeedSheikhi@outlook.com>
2019-11-30 14:19:30 +0330

    Add axios and currency

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,38 +1,98 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class Converter extends Component {
   constructor(props) {
     super(props);
     this.state = {
       toman: "",
-      dollar: ""
+      dollar: "",
+      currency: 5000,
+      loading: false
     };
   }
 
-  syncFields = e => {
-    // 1 Dollar = 11000 Toman
-    const name = e.target.name;
-    const value = e.target.value;
+  loadAsyncData = () => {
+    this.setState({ loading: true });
+    axios
+      .get("https://a.anardoni.com/pay/currency")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ currency: Number(res.data.currency) });
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        console.log(err.response);
+        this.setState({ loading: false });
+      });
+  };
+
+  componentDidMount() {
+    setTimeout(this.loadAsyncData, 1000);
+  }
+
+  syncFields = name => {
+    // 1 Dollar = 1000 Toman
+    let value;
+    const { currency, toman, dollar } = this.state;
+
+    if (name === "dollar") value = dollar;
+    if (name === "toman") value = toman;
+
     if (name === "toman") {
-      this.setState({ dollar: value / 11000 });
+      this.setState({ dollar: value / currency });
     } else if (name === "dollar") {
-      this.setState({ toman: value * 11000 });
+      this.setState({ toman: value * currency });
+    } else {
+      this.setState({
+        dollar: toman / currency
+      });
     }
   };
 
+  renderCurrencyValue = () => {
+    const { loading, currency, toman, dollar } = this.state;
+
+    if (loading) return "loading...";
+    if (!toman || !dollar) return "please enter something first";
+    return currency;
+  };
+
   render() {
+    const { loading, currency, toman, dollar } = this.state;
     return (
       <form className="form-row">
+        <div className="col-6">
+          <label>Currency - Toman</label>
+          <input
+            name="currency"
+            className="form-control"
+            type="text"
+            disabled={loading || !toman || !dollar}
+            value={this.renderCurrencyValue()}
+            onChange={e => {
+              this.setState(
+                {
+                  currency: e.target.value
+                },
+                () => {
+                  this.syncFields();
+                }
+              );
+            }}
+          ></input>
+        </div>
         <div className="col-6">
           <label>Toman</label>
           <input
             name="toman"
             className="form-control"
             type="text"
-            value={this.state.toman}
+            value={toman}
             onChange={e => {
-              this.setState({ toman: e.target.value });
-              this.syncFields(e);
+              const name = e.target.name;
+              this.setState({ toman: e.target.value }, () => {
+                this.syncFields(name);
+              });
             }}
           ></input>
         </div>
@@ -42,10 +102,12 @@ class Converter extends Component {
             name="dollar"
             className="form-control"
             type="text"
-            value={this.state.dollar}
+            value={dollar}
             onChange={e => {
-              this.setState({ dollar: e.target.value });
-              this.syncFields(e);
+              const name = e.target.name;
+              this.setState({ dollar: e.target.value }, () => {
+                this.syncFields(name);
+              });
             }}
           ></input>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
